63954906ac1d29cdafd4eac3bc140df4d7ec2471
Profiteam <31069986+Profiteam@users.noreply.github.com>
2019-01-14 14:43:22 +0300

    base entity

---
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API.Domain/Entity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.IO.Exchange.API.Domain
+{
+    public abstract class Entity<TId>
+    {
+        public virtual TId ID { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Entity<TId>);
+        }
+
+        private static bool IsTransient(Entity<TId> obj)
+        {
+            return obj != null && Equals(obj.ID, default(TId));
+        }
+
+        private Type GetUnproxiedType()
+        {
+            return GetType();
+        }
+
+        public virtual bool Equals(Entity<TId> other)
+        {
+            if (other == null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (!IsTransient(this) && !IsTransient(other) && Equals(ID, other.ID))
+            {
+                var otherType = other.GetUnproxiedType();
+                var thisType = GetUnproxiedType();
+                return thisType.IsAssignableFrom(otherType) ||
+                       otherType.IsAssignableFrom(thisType);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            if (Equals(ID, default(TId)))
+                return base.GetHashCode();
+            return ID.GetHashCode();
+        }
+    }
+}
