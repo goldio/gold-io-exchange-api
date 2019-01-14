60b6f0a28b6f76e0242e30ccc612cfa7b4024a1c
Profiteam <31069986+Profiteam@users.noreply.github.com>
2019-01-14 14:45:29 +0300

    persistent object, ideletable object

---
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API.Domain/PersistentObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.IO.Exchange.API.Domain
+{
+    public abstract class PersistentObject : Entity<long>
+    {
+    }
+}
