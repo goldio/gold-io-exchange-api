8c0ecf843a0f5a2814f3a17ff05a44c026a9d8c0
Profiteam <31069986+Profiteam@users.noreply.github.com>
2019-01-16 17:20:28 +0300

    add view models

---
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/ViewModels/PersonViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Gold.IO.Exchange.API.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gold.IO.Exchange.API.ViewModels
+{
+    public class PersonViewModel
+    {
+        public UserViewModel User { get; set; }
+
+        public long ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+
+        public PersonViewModel() { }
+
+        public PersonViewModel(Person person)
+        {
+            ID = person.ID;
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            BirthDate = person.BirthDate;
+            Email = person.Email;
+            PhoneNumber = person.PhoneNumber;
+            Country = person.Country;
+            City = person.City;
+            Address = person.Address;
+
+            User = (UserViewModel)person.User;
+        }
+    }
+}
---
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/ViewModels/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Gold.IO.Exchange.API.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gold.IO.Exchange.API.ViewModels
+{
+    public class UserViewModel
+    {
+        public long ID { get; set; }
+        public string Login { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public UserViewModel() { }
+
+        public UserViewModel(User user)
+        {
+            ID = user.ID;
+            Login = user.Login;
+            RegistrationDate = user.RegistrationDate;
+        }
+
+        public static explicit operator UserViewModel(User user) => new UserViewModel(user);
+    }
+}
