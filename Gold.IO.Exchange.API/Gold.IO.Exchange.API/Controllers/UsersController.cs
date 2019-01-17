15f7e527b7d5a06004fad4a3c07dbe6b9beb2912
Profiteam <31069986+Profiteam@users.noreply.github.com>
2019-01-17 13:28:17 +0300

    add sign up models

---
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gold.IO.Exchange.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        [HttpPost("sign-up")]
+        public async Task<IActionResult> UserSignUp()
+        {
+
+        }
+    }
+}
---
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/ViewModels/Request/SignUpRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gold.IO.Exchange.API.ViewModels.Request
+{
+    public class SignUpRequest
+    {
+        public string FullName { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
