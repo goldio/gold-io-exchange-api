246b5a2da4d5420fbdea85ddcd637feb695b73c9
Profiteam <31069986+Profiteam@users.noreply.github.com>
2019-01-17 13:33:58 +0300

    add md5 create method & identity method

--- Gold.IO.Exchange.API/Gold.IO.Exchange.API/Controllers/UsersController.cs
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/Controllers/UsersController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
+using Gold.IO.Exchange.API.BusinessLogic.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Gold.IO.Exchange.API.Controllers
@@ -10,10 +14,55 @@ namespace Gold.IO.Exchange.API.Controllers
     [ApiController]
     public class UsersController : Controller
     {
+        private IUserService UserService { get; set; }
+
+        public UsersController([FromServices]
+            IUserService userService)
+        {
+            UserService = userService;
+        }
+
         [HttpPost("sign-up")]
         public async Task<IActionResult> UserSignUp()
         {
 
+        }
+
+        private ClaimsIdentity GetIdentity(string login, string password)
+        {
+            var user = UserService.GetAll().FirstOrDefault(x => x.Login == login && x.Password == password);
+            if (user == null)
+                return null;
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.ToString())
+            };
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(
+                claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+
+            return claimsIdentity;
+        }
+
+        private string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
         }
     }
 }
