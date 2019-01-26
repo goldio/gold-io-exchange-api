638fae4071658fdd6a045061fa383e9b6888ef2b
Nazar Vasheniak <nazar.vasheniak@gmail.com>
2019-01-26 15:33:40 +0300

    add cities crud

--- Gold.IO.Exchange.API/Gold.IO.Exchange.API/Controllers/CitiesController.cs
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/Controllers/CitiesController.cs
@@ -1,5 +1,10 @@
 ﻿using Gold.IO.Exchange.API.BusinessLogic.Interfaces;
+using Gold.IO.Exchange.API.ViewModels;
+using Gold.IO.Exchange.API.ViewModels.Response;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Gold.IO.Exchange.API.Controllers
 {
@@ -13,6 +18,30 @@ namespace Gold.IO.Exchange.API.Controllers
             ICityService cityService)
         {
             CityService = cityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var cities = CityService.GetAll()
+                .Select(x => new CityViewModel(x))
+                .ToList();
+
+            return Json(new DataResponse<List<CityViewModel>> { Data = cities });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(long id)
+        {
+            var city = CityService.Get(id);
+            if (city == null)
+                return Json(new ResponseModel
+                {
+                    Success = false,
+                    Message = "City not found"
+                });
+
+            return Json(new DataResponse<CityViewModel> { Data = new CityViewModel(city) });
         }
     }
 }
