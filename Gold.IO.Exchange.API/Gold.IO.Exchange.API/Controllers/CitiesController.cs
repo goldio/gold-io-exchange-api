0ee1c778d75e2d1c90ecb04776a58ecd3f664fd8
Nazar Vasheniak <nazar.vasheniak@gmail.com>
2019-01-26 15:30:14 +0300

    add cities controller

---
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Gold.IO.Exchange.API.BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gold.IO.Exchange.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : Controller
+    {
+        private ICityService CityService { get; set; }
+
+        public CitiesController([FromServices]
+            ICityService cityService)
+        {
+            CityService = cityService;
+        }
+    }
+}
