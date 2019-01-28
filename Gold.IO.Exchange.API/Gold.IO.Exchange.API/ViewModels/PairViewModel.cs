289cc8b1fe155eb599da4fefa9f1eda2b395e8c2
Nazar Vasheniak <nazar.vasheniak@gmail.com>
2019-01-28 16:22:05 +0300

    get pairs

---
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/ViewModels/PairViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gold.IO.Exchange.API.ViewModels
+{
+    public class PairViewModel
+    {
+        public string Symbol { get; set; } // e.g. BTC/USDT
+        public CoinViewModel BaseAsset { get; set; }
+        public CoinViewModel QuoteAsset { get; set; }
+
+        public PairViewModel() { }
+
+        public PairViewModel(CoinViewModel baseAsset, CoinViewModel quoteAsset)
+        {
+            BaseAsset = baseAsset;
+            QuoteAsset = quoteAsset;
+            Symbol = $"{BaseAsset.ShortName}/{QuoteAsset.ShortName}";
+        }
+    }
+}
