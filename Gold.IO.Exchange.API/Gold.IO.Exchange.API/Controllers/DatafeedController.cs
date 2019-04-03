d792b58761e4955d96c13d97113b61fbf49d66d3
nazarvasheniak <nazar.vasheniak@gmail.com>
2019-04-03 13:03:51 +0300

    Update DatafeedController.cs

--- Gold.IO.Exchange.API/Gold.IO.Exchange.API/Controllers/DatafeedController.cs
+++ Gold.IO.Exchange.API/Gold.IO.Exchange.API/Controllers/DatafeedController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Gold.IO.Exchange.API.BusinessLogic.Interfaces;
+using Gold.IO.Exchange.API.Domain.Enum;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -35,6 +36,86 @@ namespace Gold.IO.Exchange.API.Controllers
                 supports_search = true,
                 supports_timescale_marks = false
             });
+        }
+
+        [HttpGet("history")]
+        public async Task<IActionResult> GetHistory([FromQuery] string symbol)
+        {
+            var coins = symbol.Split(".");
+
+            var baseAsset = CoinService.GetAll()
+                .FirstOrDefault(x => x.ShortName.Equals(coins[0]));
+
+            var quoteAsset = CoinService.GetAll()
+                .FirstOrDefault(x => x.ShortName.Equals(coins[1]));
+
+            var orders = OrderService.GetAll()
+                .Where(x => x.BaseAsset == baseAsset &&
+                    x.QuoteAsset == quoteAsset &&
+                    x.Status == OrderStatus.Closed)
+                    .ToList();
+
+            if (orders == null || orders.Count == 0)
+                return Ok(new
+                {
+                    s = "no_data"
+                });
+
+            var startTime = (int)(DateTime.UtcNow.Subtract(orders.Min(x => x.Time)).TotalSeconds);
+            var endTime = (int)(DateTime.UtcNow.Subtract(orders.OrderBy(x => x.Time).First().Time).TotalSeconds);
+            var fiveMinutes = 5 * 60 * 1000;
+
+            var t = new List<int>() { startTime };
+            var tempT = startTime;
+
+            while (tempT < endTime)
+            {
+                t.Add(tempT + fiveMinutes);
+                tempT += fiveMinutes;
+            }
+
+            var o = new List<double>();
+            for (var i = 0; i < t.Count; i++)
+            {
+                if (i != t.Count - 1)
+                    o.Add(orders.FirstOrDefault(x => x.Time >= UnixTimeStampToDateTime(t[i]) && 
+                        x.Time <= UnixTimeStampToDateTime(t[i + 1]))
+                    .Price);
+            }
+
+            var c = new List<double>();
+            for (var i = 0; i < t.Count; i++)
+            {
+                c.Add(orders.AsEnumerable().LastOrDefault(x => x.Time >= UnixTimeStampToDateTime(t[i]) &&
+                    x.Time <= UnixTimeStampToDateTime(t[i + 1]))
+                .Price);
+            }
+
+            var h = new List<double>();
+            for (var i = 0; i < t.Count; i++)
+            {
+                h.Add(orders.Where(x => x.Time >= UnixTimeStampToDateTime(t[i]) &&
+                    x.Time <= UnixTimeStampToDateTime(t[i + 1]))
+                    .Max(x => x.Price));
+            }
+
+            var l = new List<double>();
+            for (var i = 0; i < t.Count; i++)
+            {
+                l.Add(orders.Where(x => x.Time >= UnixTimeStampToDateTime(t[i]) &&
+                    x.Time <= UnixTimeStampToDateTime(t[i + 1]))
+                    .Min(x => x.Price));
+            }
+
+            var v = new List<double>();
+            for (var i = 0; i < t.Count; i++)
+            {
+                v.Add(orders.Where(x => x.Time >= UnixTimeStampToDateTime(t[i]) &&
+                    x.Time <= UnixTimeStampToDateTime(t[i + 1]))
+                    .Sum(x => x.Amount));
+            }
+
+            return Ok(new { t, o, c, h, l, v, s = "ok" });
         }
 
         [HttpGet("symbols")]
@@ -113,6 +194,14 @@ namespace Gold.IO.Exchange.API.Controllers
 
             [JsonProperty("type")]
             public string Type { get; set; } = "bitcoin";
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
         }
     }
 }
